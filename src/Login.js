1a9ed3ac9fd731f8f07731bcd2b73d02cec7c451
Ankit Kumar <ankit07roykroos@gmail.com>
2022-11-26 22:12:44 +0530

    started login page with no route match location error

--- src/Login.js
+++ src/Login.js
@@ -5,6 +5,7 @@ function Login() {
   return (
     <div className='login'>
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' alt='' />
+        <h1>this is my life</h1>
     </div>
   )
 }
