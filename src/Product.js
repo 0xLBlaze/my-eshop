b88f7d710b6c3944900c6fa828912ec9ac123e62
Ankit Kumar <ankit07roykroos@gmail.com>
2022-11-26 12:06:12 +0530

    first commit

---
+++ src/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Product.css';
+
+function Product({id, title, image, price, rating},pos) {
+  return (
+    <div className='product'>
+        <div className="product__info">
+            <p key={pos}>{title}</p>
+            <p className='product__price'>
+                <small>$</small>
+                <strong>{price}</strong>
+            </p>
+            <div className="product__rating">
+              {Array(rating).fill().map((_,i) =>(
+                <p>🌟</p>
+              ))}
+                
+            </div>
+        </div>
+        <img src={image} alt="" />
+        <button>Addo to Basket</button>
+    </div>
+  )
+}
+
+export default Product
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQJls_KGGnSCE9bqwk1k2QBQAbb1ZsNn8",
+    authDomain: "clone-e50ff.firebaseapp.com",
+    projectId: "clone-e50ff",
+    storageBucket: "clone-e50ff.appspot.com",
+    messagingSenderId: "1090949392792",
+    appId: "1:1090949392792:web:b3b49118df7f6ab1f963ed"
+  };
