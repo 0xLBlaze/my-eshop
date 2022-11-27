6b6b2a5f472394e4f8cbb973d67b0bf0dce1ebb1
Ankit Kumar <ankit07roykroos@gmail.com>
2022-11-27 22:35:56 +0530

    27nov-login-page-done

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCQJls_KGGnSCE9bqwk1k2QBQAbb1ZsNn8",
     authDomain: "clone-e50ff.firebaseapp.com",
@@ -6,3 +10,9 @@ const firebaseConfig = {
     messagingSenderId: "1090949392792",
     appId: "1:1090949392792:web:b3b49118df7f6ab1f963ed"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {db, auth};
